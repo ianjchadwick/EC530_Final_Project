436b0b9c327fbc3356b81d8d78032e65ed8532ac
Ian Chadwick <ianjchadwick@gmail.com>
2022-05-16 13:51:30 -0700

    Basic html templates

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 # Stores all the views that the user can navigate to, except the authentication page which is in auth
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
 

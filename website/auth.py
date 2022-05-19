512dc97207e875f77e795d4264ce517a0173b4fe
Ian Chadwick <ianjchadwick@gmail.com>
2022-05-19 15:25:40 -0700

    Fixed the data models to have separate contact and role tables

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from.models import User
+from.models import User, Role, Contact
 from werkzeug.security import generate_password_hash, check_password_hash
 from. import db
 from flask_login import login_user, login_required, logout_user, current_user
@@ -38,25 +38,23 @@ def logout():
 def register():
     # Get the info from the sign-up form
     if request.method == 'POST':
+        # Get info for user table
         email = request.form.get('email')
         first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        DOB = request.form.get('DOB')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        role1 = request.form.get('role1')
-        role2 = request.form.get('role2')
-        role3 = request.form.get('role3')
-        if role1 == 'patient':
-            patient = True
-        else:
-            patient = False
-        if role2 == 'doctor':
-            doctor = True
-        else:
-            doctor = False
-        if role3 == 'doctor':
-            admin = True
-        else:
-            admin = False
+        # Get role table information
+        patient = request.form.get('role1')
+        doctor = request.form.get('role2')
+        admin = request.form.get('role3')
+        # Get contact table information
+        phone = request.form.get('phone')
+        street = request.form.get('street')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        zip = request.form.get('zip')
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists.', category='error')
@@ -69,18 +67,33 @@ def register():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category="error")
         elif not (admin or doctor or patient):
-            flash('You must select at least one role!', category="error")
+            flash('You must select at least one role.', category="error")
         else:
             # add user to database
             new_user = User(email=email,
                             first_name=first_name,
-                            password=generate_password_hash(password1, method='sha256'),
-                            patient=patient,
-                            doctor=doctor,
-                            admin=admin)
+                            last_name=last_name,
+                            DOB=DOB,
+                            password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            # Log the user in so we can get their newly assigned id
             login_user(new_user, remember=True)
+            # Assign user roles
+            new_user_roles = Role(doctor=doctor,
+                                  admin=admin,
+                                  patient=patient,
+                                  user_id=current_user.id)
+            db.session.add(new_user_roles)
+            # Add new user contact information
+            new_user_contact = Contact(phone=phone,
+                                       street=street,
+                                       city=city,
+                                       state=state,
+                                       zip=zip,
+                                       user_id=current_user.id)
+            db.session.add(new_user_contact)
+            db.session.commit()
             flash('Account created!', category="success")
             return redirect(url_for('views.home'))
 
--- website/models.py
+++ website/models.py
@@ -15,10 +15,29 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    patient = db.Column(db.Boolean)
-    doctor = db.Column(db.Boolean)
-    admin = db.Column(db.Boolean)
+    last_name = db.Column(db.String(150))
+    DOB = db.Column(db.String(8))
+    roles = db.relationship('Role')
     notes = db.relationship('Note')
+    contact = db.relationship('Contact')
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    doctor = db.Column(db.String(10))
+    patient = db.Column(db.String(10))
+    admin = db.Column(db.String(10))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone = db.Column(db.String(10))
+    street = db.Column(db.String(150))
+    city = db.Column(db.String(150))
+    state = db.Column(db.String(2))
+    zip = db.Column(db.String(5))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class Device(db.Model):
